71d5fb1498fc51220f47a3afa04073991c56a6dc
jacobmcf <jacobmcf@student.ubc.ca>
2021-02-26 09:25:19 -0800

    Add docstring to tidy_kmeans

--- sktidy/sktidy.py
+++ sktidy/sktidy.py
@@ -2,7 +2,39 @@
 def tidy_lr():
 
 #Jacob
-def tidy_kmeans():
+    """
+    Return a tidy df of cluster information for a kmeans clustering algorithm
+
+    Parameters
+    ----------
+    model : sklearn.cluster.KMeans
+        The model to extract the cluster specific information from. 
+    dataframe : pandas dataframe
+        The data to which the Kmeans object has been fitted
+
+    Returns
+    -------
+    df : pandas dataframe
+        A dataframe with k rows, where k is the number of clusters and 3 columns,
+        describing respectively the center of the cluster, the sum of inertia of the
+        cluster, and the number of associated data points in a cluster.
+
+    Examples
+    --------
+    These are written in doctest format, and should illustrate how to
+    use the function.
+
+    a = [1, 2, 3]
+    print([x + 3 for x in a])
+    [4, 5, 6]
+    print("a\nb")
+    a
+    b
+    """
+def tidy_kmeans(kmeans):
+
+    pass
+
 
 #Heidi
 def augment_lr():
