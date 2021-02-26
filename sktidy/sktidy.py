64656762fea600ff5b85d4eaed2d4f48c933e734
Heidi Ye <heidiye@students.ubc.ca>
2021-02-26 09:30:24 -0800

    Adding doc string for augment_lr()

--- sktidy/sktidy.py
+++ sktidy/sktidy.py
@@ -5,7 +5,24 @@ def tidy_lr():
 def tidy_kmeans():
 
 #Heidi
-def augment_lr():
+def augment_lr(X,y):
+    """
+    Adds two columns to the original data of the linear regression model. This includes predictions and residuals.
+
+    Parameters
+    ----------
+    X : pandas.core.frame.DataFrame
+        A dataframe of explanatory variables to predict on. Shaped n observations by m features.
+
+    y : pandas.core.frame.DataFrame
+        A dataframe of response variables to predict on. Shaped n observations by 1.
+
+    Returns
+    -------
+    df : pandas.core.frame.DataFrame
+        A dataframe with the original data plus two additional columns for predictions and residuals. Shaped n observations by m features + 2.
+
+    """
 
 #Asma
 def augment_kmeans():
