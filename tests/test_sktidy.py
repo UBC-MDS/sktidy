2e1dbe03284f327ef5b37c10d58336f0d1d50820
jacobmcf <jacobmcf@student.ubc.ca>
2021-03-05 15:39:49 -0800

    Add tests for exceptions

--- tests/test_sktidy.py
+++ tests/test_sktidy.py
@@ -21,10 +21,7 @@ def KMeans_out_data():
     kmeans_tidy_output = pd.DataFrame({"cluster_number" : np.array([0,1]),
                                        "cluster_inertia" : np.array([8,8]),
                                        "n_points" : np.array([4,4])})
-    return kmeans_tidy_output
-                        
-
-
+    return kmeans_tidy_output                      
 
 def test_version():
     assert __version__ == '0.1.0'
@@ -55,11 +52,31 @@ def test_tidy_lr():
         sktidy.tidy_lr(model = my_lr_2, X = X, y = y)
 
 def test_tidy_kmeans(KMeans_in_data, KMeans_out_data):
+    # Fitting a KMeans model for testing
     kmeans_test = KMeans(n_clusters = 2)
     kmeans_test.fit(KMeans_in_data)
-    
-    
 
+    # Getting our sktidy output
+    tidy_output = sktidy.tidy_kmeans(model = kmeans_test, dataframe = KMeans_in_data) 
+
+    # Comparing it with our expected outputs
+    tidy_output.equals(KMeans_out_data)
+
+    # Checking that we raise a type error when the wrong model is input
+    X = datasets.load_iris(return_X_y = True, as_frame = True)[0]
+    y = datasets.load_iris(return_X_y = True, as_frame = True)[1]
+    z = np.random.rand(X.shape[1])
+
+    my_lr = LinearRegression()
+    my_lr.fit(X,y)
+    with raises(TypeError):
+        sktidy.tidy_kmeans(model = my_lr, dataframe = KMeans_in_data)
+    
+    # Creating an untrained model and checking that we raise a NotFittedError when we try to use tidy on it
+    with raises(NotFittedError):
+        sktidy.tidy_kmeans(model = KMeans(), dataframe = KMeans_in_data)
+
+    
 
 def test_augment_lr():
     X = datasets.load_iris(return_X_y = True, as_frame = True)[0]
